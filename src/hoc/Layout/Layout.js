53209bbaebdf9de204b1fe210417ff5a7bde55ce
rayzhou-bit <rayzhou12.12@gmail.com>
2021-02-13 10:51:55 -0800

    Initial commit

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {connect} from 'react-redux';
+
+import classes from './Layout.css';
+import Auxiliary from '../Auxiliary';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+
+const layout = props => {
+  const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setSideDrawerIsVisible(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setSideDrawerIsVisible(!sideDrawerIsVisible);
+  };
+
+  return (
+    <Auxiliary>
+      <Toolbar
+        isAuth={props.isAuthenticated}
+        drawerToggleClicked={sideDrawerToggleHandler} />
+      <SideDrawer
+        isAuth={props.isAuthenticated}
+        open={sideDrawerIsVisible}
+        closed={sideDrawerClosedHandler} />
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </Auxiliary>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !=null,
+  };
+};
+
+export default connect(mapStateToProps,null)(layout);
